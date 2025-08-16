20c939a89df2aa5835eb55c24364646464ae7563
Rafael <RafaelEbenHart_2327240120@mhs.mdp.ac.id>
2025-08-16 21:31:17 +0700

    update

--- binaryClassification.py
+++ binaryClassification.py
@@ -10,7 +10,6 @@ from pathlib import Path
 
 # make 1000 samples
 nSamples = 1000
-
 X, y = make_circles(nSamples,
                     noise=0.03,
                     random_state=42)
@@ -26,10 +25,10 @@ circles = pd.DataFrame({"X1": X[:,0],
 print(circles.head(10))
 
 # membuat graph
-plt.scatter(x=X[:,0],
-            y=X[:,1],
-            c=y,
-            cmap=plt.cm.RdYlBu);
+# plt.scatter(x=X[:,0],
+#             y=X[:,1],
+#             c=y,
+#             cmap=plt.cm.BkYlGy);
 # plt.show()
 ## data yang di kerjakan diatas biasa disebut juga toy data set
 
@@ -93,11 +92,20 @@ print(next(model0.parameters()).device)
 
 # replikasi model
 model0 = nn.Sequential(
-    nn.Linear(in_features= 2, out_features= 5),
-    nn.Linear(in_features= 5, out_features=1)
+    nn.Linear(in_features= 2, out_features= 10),
+    nn.ReLU(),
+    nn.Linear(in_features= 10, out_features=20),
+    nn.ReLU(),
+    nn.Linear(in_features=20, out_features=1)
 ).to(device)
 print(model0)
-# menghasilkan model yang sama dengan model sebelumnya
+# menghasilkan model yang sama dengan model sebelumnya dengan nn.Sequential()
+## Note:
+# replikasi dengan nn.Sequential model ini sudah di perbaharui dengan penambahan layer
+# dan method nn.ReLU(),dengan demikian hasil yang didapatkan luar biasa
+
+# namun model yang didefinisikan di awal yakni CircleModelV0 belum di perbaiki dan hasilnya tidak memuaskan
+# sekedar menjadi contoh jika mengalami kasus yang sama
 
 # visualisasi dari prediksi model
 print(model0.state_dict())
@@ -108,7 +116,7 @@ print(f"len dari test sampel: {len(XTest)}, Shape: {yTest.shape}")
 print(f"\n10 pred pertama:\n {torch.round(untrainedPreds[:10])}")
 print(f"\n10 label dari pred:\n {yTest[:10]}")
 
-# mendefinisikan loss function dan optimizer
+## 3 mendefinisikan loss function dan optimizer
 # loss func
 # l1Loss / MAE(mean absolut error) berkerja untuk linear manun tidak untuk classification
 # loss function yang digunakan untuk classification biasanya adalah:
@@ -121,8 +129,8 @@ lossFunc = torch.nn.BCEWithLogitsLoss()
 
 # optimizer
 # untuk optimizer yang biasa di gunakana adalah adam atau SGD
-optimizer = torch.optim.SGD(params=model0.parameters(),
-                            lr= 0.1)
+optimizer = torch.optim.Adam(params=model0.parameters(),
+                            lr= 0.01)
 
 # mengkalkulasi akurasi - menghtiung dari 100 sampel berapa persen model benar
 def accuracyFN(yTrue, yPred):
@@ -168,7 +176,7 @@ yPredLabels = torch.round(torch.sigmoid(model0(XTest.to(device))[:5]))
 # cek kesamaan
 print(torch.eq(yPreds.squeeze(), yPredLabels.squeeze()))
 
-# membuat training,testing loop dan evaluation loop
+## 4 membuat training,testing loop dan evaluation loop
 torch.cuda.manual_seed(42)
 epochs = 100
 # epochCount = []
@@ -177,7 +185,7 @@ epochs = 100
 
 XTrain.to(device),yTrain.to(device)
 XTest.to(device),yTrain.to(device)
-
+## note: contoh to(device) ini salah,contoh benar ada dibawah, model2
 
 for epoch in range(epochs):
     # forward pass
@@ -214,26 +222,160 @@ for epoch in range(epochs):
                              yPred=testPred)
         # print
         if epoch % 10 == 0:
-            print(f"| Epoch: {epoch}/{epochs} | loss: {loss:.5f} Acc: {Acc:.2f}% | Test loss: {testLoss:.5f} | Test Acc: {testAcc:.2f}% |")
+            print(f"| Epoch: {epoch}/{epochs} | loss: {loss:.5f} | Acc: {Acc:.2f}% | Test loss: {testLoss:.5f} | Test Acc: {testAcc:.2f}% |")
 
 # membuat visual predicitons
-if Path("Helper_funtion.py").is_file():
-    print("Already exists")
-else:
-    print("downloading")
-    requests = requests.get("https://raw.githubusercontent.com/mrdbourke/pytorch-deep-learning/refs/heads/main/helper_functions.py")
-    with open("helper_function.py","wb") as f:
-        f.write(requests.content)
+# if Path("Helper_funtion.py").is_file():
+#     print("Already exists")
+# else:
+#     print("downloading")
+#     requests = requests.get("https://raw.githubusercontent.com/mrdbourke/pytorch-deep-learning/refs/heads/main/helper_functions.py")
+#     with open("helper_function.py","wb") as f:
+#         f.write(requests.content)
 
 from helper_function import plot_predictions, plot_decision_boundary
 
-plt.figure(figsize=(10,5))
-plt.subplot(1, 2, 1)
-plt.title("Train")
-plot_decision_boundary(model0, XTrain, yTrain)
-plt.subplot(1, 2, 2)
-plt.title("Test")
-plot_decision_boundary(model0, XTest, yTest)
+# plt.figure(figsize=(10,5))
+# plt.subplot(1, 2, 1)
+# plt.title("Train")
+# plot_decision_boundary(model0, XTrain, yTrain)
+# plt.subplot(1, 2, 2)
+# plt.title("Test")
+# plot_decision_boundary(model0, XTest, yTest)
 # plt.show()
 
-print(XTrain.squeeze().shape)
+
+## 5 memperbaiki model dari prespektif model
+# opsi:
+# menambah layer baru pada model untuk memberikan kesempatan model untuk mempelajari pola
+# menambah hidden units atau neuron / dari 5 units ke 10 units
+# menambah epochs
+# merubah activation function
+# merubah learning rate
+# merubah loss function
+
+
+## Membuat model versi 1 dari CircleModelV0 dengan mengunnakn subclass
+# Dengan perubahan pada model meliputi:
+# 1.Menambah layer tambahan
+# 2.Menambah hidden units
+# 3.Menambah jumlah epochs
+
+class CircleModelV1(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer1 = nn.Linear(in_features=2, out_features=10)
+        self.layer2 = nn.Linear(in_features=10,out_features=10)
+        self.layer3 = nn.Linear(in_features=10,out_features=1)
+    def forward(self,x):
+        return self.layer3(self.layer2(self.layer1(x)))
+
+model1 = CircleModelV1().to(device)
+print(next(model1.parameters()).device)
+
+lossFuncModel1 = torch.nn.BCEWithLogitsLoss()
+optimizerModel1 = torch.optim.SGD(params=model1.parameters(),
+                                 lr= 0.01)
+
+# torch.cuda.manual_seed(42)
+# epochsModel1 = 1000
+
+# for epoch in range(epochsModel1):
+#     yLogitsModel1 = model1(XTrain.to(device)).squeeze()
+#     ypredModel1 = torch.round(torch.sigmoid(yLogitsModel1))
+
+#     lossModel1 = lossFuncModel1(ypredModel1,yTrain.to(device))
+#     AccModel1 = accuracyFN(yTrain,ypredModel1)
+
+#     optimizerModel1.zero_grad()
+#     lossModel1.backward()
+#     optimizerModel1.step()
+
+#     model1.eval()
+#     with torch.inference_mode():
+#         testLogitsModel1 = model1(XTest.to(device)).squeeze()
+#         testPredModel1 = torch.round(torch.sigmoid(testLogitsModel1))
+
+#         testLossModel1 = lossFuncModel1(testLogitsModel1.to(device),yTest.to(device))
+#         testAccModel1 = accuracyFN(yTest,testPredModel1)
+
+#         if epoch % 100 == 0:
+#          print(f"| Epochs: {epoch}/{epochsModel1} | Loss Model1: {lossModel1:.5f} | Acc Model1: {AccModel1:.2f}% | Test loss: {testLossModel1:.5f} | Test Acc: {testAccModel1:.2f}% |")
+
+# plt.figure(figsize=(10,5))
+# plt.subplot(1, 2, 1)
+# plt.title("Train")
+# plot_decision_boundary(model1, XTrain, yTrain)
+# plt.subplot(1, 2, 2)
+# plot_decision_boundary(model1, XTest, yTest)
+# plt.title("Test")
+# plt.show()
+
+## 5.1 menyiapkan data untuk melihat apakah modelV1 bisa memprediksi straight line
+# cara untuk menemukan permasalahan yang besar,adalah dengan melakukan test pada masalah kecil terlebih dahulu
+
+# membuat data yang sama seperti "01"
+weight = 0.7
+bias = 0.3
+start = 0
+end = 1
+step = 0.01
+XRegresi = torch.arange(start, end, step).unsqueeze(dim=1)
+yRegresi = weight * XRegresi + bias # membuat rumus regresi linear
+
+
+# membuat trainsplits
+trainSplit = int(0.8* len(XRegresi))
+XTrainReg,yTrainReg = XRegresi[:trainSplit], yRegresi[:trainSplit]
+XTestReg,yTestReg = XRegresi[trainSplit:], yRegresi[trainSplit:]
+
+plot_predictions(train_data=XTrainReg,
+                 train_labels=yTrainReg,
+                 test_data=XTestReg,
+                 test_labels=yTestReg,
+                 predictions=None)
+
+## 5.2 membuat model1 bisa memprediksi straight line data
+# mengubah in_feature yang tadinya 2 menjadi 1 dengan nn.Sequential karen input regresi hanya 1
+
+model2 = nn.Sequential(
+    nn.Linear(in_features=1, out_features=10),
+    nn.Linear(in_features=10, out_features=10),
+    nn.Linear(in_features=10 , out_features=1)
+).to(device)
+
+lossFuncModel2 = nn.L1Loss()
+optimizerModel2 = torch.optim.SGD(params= model2.parameters(),
+                            lr=0.01)
+
+# train model
+torch.cuda.manual_seed(42)
+epochsModel2 = 1000
+XTrainReg,yTrainReg =XTrainReg.to(device),yTrainReg.to(device)
+XTestReg,yTestReg = XTestReg.to(device),yTestReg.to(device)
+# print(XTrainReg.device)
+
+# Training
+for epoch in range(epochsModel2):
+    yPredReg = model2(XTrainReg)
+    lossModel2 = lossFuncModel2(yPredReg,yTrainReg)
+    optimizerModel2.zero_grad()
+    lossModel2.backward()
+    optimizerModel2.step()
+
+    # Testing
+    model2.eval()
+    with torch.inference_mode():
+        testPredModel2 = model2(XTestReg)
+        testLossModel2 = lossFuncModel2(testPredModel2,yTestReg)
+
+    if epoch % 100 == 0:
+        print(f"epochs: {epoch}/{epochsModel2} | Train loss:  {lossModel2:.5f} | Test loss: {testLossModel2:.5f}")
+
+XTrainReg,yTrainReg = XTrainReg.cpu().numpy(),yTrainReg.cpu().numpy()
+yTestReg = yTestReg.cpu().numpy()
+testPredModel2 = testPredModel2.cpu().numpy()
+with torch.inference_mode():
+    testPredModel2 = model2(XTestReg)
+plot_predictions(XTrainReg,yTrainReg,XTestReg.cpu().numpy(),yTestReg,testPredModel2)
+plt.show()
